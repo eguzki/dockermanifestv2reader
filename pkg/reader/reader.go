dc8c8c141069a61960002f2b06d56b5e67376e58
Eguzki Astiz Lezaun <eastizle@redhat.com>
2020-02-23 11:18:44 +0100

    support registry.redhat.io and quay.io

--- pkg/reader/reader.go
+++ pkg/reader/reader.go
@@ -1,8 +1,10 @@
 package reader
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
@@ -20,7 +22,7 @@ func Read(imageURL string) error {
 
 	registryURL, repository, tag := parseImageURL(imageURL)
 
-	hub, err := registry.New(registryURL, username, password)
+	hub, err := registry.NewInsecure(registryURL, username, password)
 	if err != nil {
 		return err
 	}
@@ -30,12 +32,8 @@ func Read(imageURL string) error {
 	if err != nil {
 		return err
 	}
-	req.Header.Add("Accept", "application/vnd.docker.distribution.manifest.v2+json")
 
-	// do not follow redirects - this is critical so we can get the registry digest from Location in redirect response
-	hub.Client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
-		return http.ErrUseLastResponse
-	}
+	req.Header.Add("Accept", "application/vnd.docker.distribution.manifest.list.v2+json, application/vnd.docker.distribution.manifest.v2+json")
 
 	resp, err := hub.Client.Do(req)
 	if err != nil {
@@ -46,25 +44,109 @@ func Read(imageURL string) error {
 		defer resp.Body.Close()
 	}
 
-	if resp.StatusCode != 302 && resp.StatusCode != 301 {
-		return fmt.Errorf("returned statusCode: %v", resp.StatusCode)
-	}
-
-	digestURL, err := resp.Location()
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
-	if digestURL == nil {
-		return errors.New("digestURL is nil")
-	}
-	digestURLParts := strings.Split(digestURL.EscapedPath(), "/")
-	if len(digestURLParts) < 1 {
-		return fmt.Errorf("digestURL can not be splitted: %s", digestURL)
+
+	//fmt.Println(string(body))
+
+	//for k, v := range resp.Header {
+	//	fmt.Print(k)
+	//	fmt.Print(" : ")
+	//	fmt.Println(v)
+	//}
+
+	if len(resp.Header["Content-Type"]) < 1 {
+		return errors.New("Content-type header not found")
 	}
 
-	fmt.Println(digestURLParts[len(digestURLParts)-1])
+	switch contentTypeHeader := resp.Header["Content-Type"][0]; contentTypeHeader {
+	case "application/vnd.docker.distribution.manifest.list.v2+json":
+		digest, err := parseFatManifest(body)
+		if err != nil {
+			return err
+		}
+		fmt.Println(digest)
+	case "application/vnd.docker.distribution.manifest.v2+json":
+		digest, err := parseManifestV2Headers(resp.Header)
+		if err != nil {
+			return err
+		}
+		fmt.Println(digest)
+	default:
+		return fmt.Errorf("Content-type not known: %s", contentTypeHeader)
+	}
 
 	return nil
+}
+
+func parseFatManifest(body []byte) (string, error) {
+	//{
+	//	"manifests": [
+	//	{
+	//		"digest": "sha256:de3ab628b403dc5eed986a7f392c34687bddafee7bdfccfd65cecf137ade3dfd",
+	//		"mediaType": "application/vnd.docker.distribution.manifest.v2+json",
+	//		"platform": {
+	//			"architecture": "amd64",
+	//			"os": "linux"
+	//		},
+	//		"size": 1160
+	//	},
+	//	{
+	//		"digest": "sha256:53215627779289c622f99744e546ec8a73b334bf63a19aa1a1db0f36a2e87d13",
+	//		"mediaType": "application/vnd.docker.distribution.manifest.v2+json",
+	//		"platform": {
+	//			"architecture": "ppc64le",
+	//			"os": "linux"
+	//		},
+	//		"size": 1160
+	//	},
+	//	{
+	//		"digest": "sha256:b14915ab40ba42f7cc353da51f49490675d6920e0c1e86d6ec99bf5873ca2df3",
+	//		"mediaType": "application/vnd.docker.distribution.manifest.v2+json",
+	//		"platform": {
+	//			"architecture": "s390x",
+	//			"os": "linux"
+	//		},
+	//		"size": 1160
+	//	}
+	//	],
+	//	"mediaType": "application/vnd.docker.distribution.manifest.list.v2+json",
+	//	"schemaVersion": 2
+	//}
+	//
+
+	var result map[string]interface{}
+	err := json.Unmarshal(body, &result)
+	if err != nil {
+		return "", err
+	}
+
+	manifests := result["manifests"].([]interface{})
+
+	digest := ""
+	for _, item := range manifests {
+		manifest := item.(map[string]interface{})
+		platform := manifest["platform"].(map[string]interface{})
+		architecture := platform["architecture"].(string)
+		if architecture == "amd64" {
+			digest = manifest["digest"].(string)
+		}
+	}
+	if digest == "" {
+		return "", errors.New("Digest not found for amd64 architecture")
+	}
+
+	return digest, nil
+}
+
+func parseManifestV2Headers(headers http.Header) (string, error) {
+	if len(headers["Docker-Content-Digest"]) < 1 {
+		return "", errors.New("Expected Docker-Content-Digest header not found")
+	}
+
+	return headers["Docker-Content-Digest"][0], nil
 }
 
 //
